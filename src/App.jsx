238f132fdce77f213829c28bcbf7882d654e65e7
rumesh <rumeshk066@gmail.com>
2024-10-22 21:49:18 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
 
 function App() {
-  const a = 10;
+  // const a = 10;
+  const firstname = 'John';
+  const lastname = 'Doe';
+  const age = 30;
+  const jobTitle = 'Software Engineer';
+
+  const getFullName = (f, l) => {
+    return `${f} ${l}`;
+
+  };
+
+  const arr = ["Apple", "Microsoft"];
+
+  const skill =  <ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+  <li>React</li>
+</ul>;
+
+const bobj = {
+  name: "Bob",
+  age: 30,
+  job: "Software Engineer",
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345"
+  }
+}
 
   return (
     <>
       <div className="cont">
-        <h1>React rocks</h1>
+        {/* <h1>React rocks</h1>
         <p>{10*10}% for JSX</p>
         <p>This is a variable: {a}</p>
-        <p>{ a>50 ? "Greater" : "Lesser"}</p>
+        <p>{ a>50 ? "Greater" : "Lesser"}</p> */}
+        <h1>Employee details</h1>
+        <p>Full name: {firstname} {lastname}</p>
+        <p>Full name (function): {getFullName("Rumesh", "Kumara")}</p>
+        <p>Age: {age}</p>
+        <p>Job title: {jobTitle}</p>
+        <p>{firstname} like {arr[1]} device</p>
+        <h2>Skills</h2>
+         {skill}
+        <h2>Object</h2>
+        <p>{bobj.name}</p> 
       </div>
     </>
   )
