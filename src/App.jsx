0e1dbae7bac14b020c20b98861a6922936f56908
rumesh <rumeshk066@gmail.com>
2024-10-22 17:50:15 +0530

    Add-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 function App() {
+  const a = 10;
 
   return (
     <>
-      <h1 className="heading">React rocks</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor delectus eum qui illum officia natus omnis eveniet, enim sit quidem libero temporibus numquam adipisci iusto odit minima nobis facere rerum?</p>
+      <div className="cont">
+        <h1>React rocks</h1>
+        <p>{10*10}% for JSX</p>
+        <p>This is a variable: {a}</p>
+        <p>{ a>50 ? "Greater" : "Lesser"}</p>
+      </div>
     </>
   )
 }
