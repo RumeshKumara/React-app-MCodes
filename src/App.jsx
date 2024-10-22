d8e265cb564722ba61a0e83744f98f3f26fc38af
rumesh <rumeshk066@gmail.com>
2024-10-22 17:22:30 +0530

    Adding css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@
 function App() {
 
   return (
-    <h1>React rocks</h1>
+    <>
+      <h1 className="heading">React rocks</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor delectus eum qui illum officia natus omnis eveniet, enim sit quidem libero temporibus numquam adipisci iusto odit minima nobis facere rerum?</p>
+    </>
   )
 }
 
