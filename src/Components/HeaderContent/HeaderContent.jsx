de382f4e10443c3e302d3a9fdcc86bece3bd126a
rumesh <rumeshk066@gmail.com>
2024-10-23 11:03:08 +0530

    Add to Menu link

--- src/Components/HeaderContent/HeaderContent.jsx
+++ src/Components/HeaderContent/HeaderContent.jsx
@@ -1,10 +1,20 @@
 // import React from 'react'
 import './HeaderContent.css'
+import logo from '../../assets/react.svg'
+import MenuLink from '../MenuLink/MenuLink'
 
 function HeaderContent() {
   return (
     <div id="navcontent">
-        <h3>Header Content</h3>
+      <a href="https://github.com/RumeshKumara?tab=repositories">
+        <img src={logo} alt="React" />
+      </a>
+      <div>
+        <MenuLink linkname="Home" url="#home" />
+        <MenuLink linkname="About" url="#about"/>
+        <MenuLink linkname="Contact" url="#contact"/>
+        <MenuLink linkname="Login" url="#login"/>
+      </div>
     </div>
   )
 }
---
+++ src/Components/MenuLink/MenuLink.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import './MenuLink.css'
+function MenuLink() {
+  return (
+    <>
+      <a href="#home" className="link">Home</a>
+    </>
+  )
+}
+
+export default MenuLink
